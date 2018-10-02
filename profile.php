04cf4b087da7016fec740a844486f904205ea82c
arnaubosch16 <43789927+arnaubosch16@users.noreply.github.com>
2018-10-02 18:32:13 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -11,9 +11,10 @@
   $GetUserInformation = mysqli_query($db_con, "SELECT * FROM friends WHERE user2 = '$friendsUser' && status = 'accepted' ");
 
 
-  if(mysqli_num_rows($GetUserInformation) > 0){
+  if(mysqli_num_rows($GetUserInformation) > 0)
+	  
 
-    while($amigos = $GetUserInformation -> fetch_assoc()){
+    while($am= $GetUserInformation -> fetch_assoc()){
 
       $colegas = $amigos['user1'];
 
